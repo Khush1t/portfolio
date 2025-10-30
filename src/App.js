ae9e19edd6b8149d1909b1d95c01fa31e09e47b4
Khushwant <Khushwant@itgi.co.in>
2025-10-30 17:16:49 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-white text-gray-900 min-h-screen">
+      <h1 className="text-4xl font-bold text-center mt-20">Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
